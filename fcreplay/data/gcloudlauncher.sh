8cbe3947a4153e72656e27aa3b2fddcf86d4ff09
Gino Lisignoli <glisignoli@gmail.com>
2020-07-19 17:38:24 +1200

    Updated service details

--- fcreplay/data/gcloudlauncher.sh
+++ fcreplay/data/gcloudlauncher.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # Depending on the hostname we want to do different things.
 # Obviously this isn't the best solutions, but should work fine.
+# This file needs to put in /usr/local/bin, owned by fbarecorder
 
 # Do recording
 if [ $HOSTNAME -eq 'fcreplay-image-1' ]; then
