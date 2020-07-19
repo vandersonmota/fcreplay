1bbaba44c7dc04027e4251c12ae6b421560a48c3
Gino Lisignoli <glisignoli@gmail.com>
2020-07-19 19:58:25 +1200

    fixed startup I hope...

--- fcreplay/data/gcloudlauncher.sh
+++ fcreplay/data/gcloudlauncher.sh
@@ -4,12 +4,12 @@
 # This file needs to put in /usr/local/bin, owned by fbarecorder
 
 # Do recording
-if [[ "$HOSTNAME" == 'fcreplay-image-1' ]]; then
+if [[ "$(hostname)" == 'fcreplay-image-1' ]]; then
     startx
 fi
 
 # Do post processing
-if [[ "$HOSTNAME" == 'fcreplay-postprocessing-1' ]]; then
+if [[ "$(hostname)" == 'fcreplay-postprocessing-1' ]]; then
     cd ~/fcreplay
     source ./venv/bin/activate
     fcreplaycloudpost
