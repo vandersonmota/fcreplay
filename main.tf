28a71c24d0a8719c39d6941929c0a24fa176088a
Gino Lisignoli <glisignoli@gmail.com>
2020-07-26 20:18:21 +1200

    Slowly getting telegraf going

--- main.tf
+++ main.tf
@@ -7,53 +7,16 @@ provider "google" {
   region  = "us-central1"
   zone    = "us-central1-a"
 }
-//TODO Handle adding shutdown script
 
-//Create service accounts
-resource "google_service_account" "fcrecorder-storage-access" {
-  account_id   = "fcrecorder-storage-access"
-  display_name = "FCRecorder storage account"
-}
-
-resource "google_service_account" "fcrecorder-compute-account" {
+resource "google_service_account" "fcrecorder_compute_account" {
   account_id   = "fcrecorder-compute-account"
   display_name = "FCRecorder compute account"
 }
 
 //Add permissions to fcrecorder-compute-account
-resource "google_project_iam_policy" "project" {
-  project     = "fcrecorder"
-  policy_data = data.google_iam_policy.compute_admin.policy_data
-  depends_on  = [google_service_account.fcrecorder-compute-account]
+resource "google_project_iam_binding" "project" {
+  project = "fcrecorder"
+  role    = "roles/compute.admin"
+  members = ["serviceAccount:fcrecorder-compute-account@fcrecorder.iam.gserviceaccount.com"]
 }
 
-data "google_iam_policy" "compute_admin" {
-  binding {
-    role = "roles/compute.admin`"
-    members = ["serviceAccount:fcrecorder-compute-account@fcrecorder.iam.gserviceaccount.com"]
-  }
-}
-
-/*Add permissions to fcrecorder-storage-access
-resource "google_project_iam_policy" "project-storage" {
-  project     = "fcrecorder"
-  policy_data = data.google_iam_policy.storage_admin.policy_data
-  depends_on = [google_service_account.fcrecorder-storage-access]
-}
-
-data "google_iam_policy" "storage_admin" {
-  binding {
-    role = "roles/roles/storage.admin"
-    members = [
-      "user:fcrecorder-storage-access@fcrecorder.iam.gserviceaccount.com",
-    ]
-  }
-}
-*/
-
-resource "null_resource" "get_storage_key" {
-  provisioner "local-exec" {
-    command = "gcloud iam service-accounts keys create .storage_creds.json --iam-account fcrecorder-storage-access@fcrecorder.iam.gserviceaccount.com"
-  }
-  depends_on = [google_service_account.fcrecorder-storage-access]
-}
