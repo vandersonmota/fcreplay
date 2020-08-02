c78361e3b916390eed1b30f1038c25798126d194
Gino Lisignoli <glisignoli@gmail.com>
2020-08-02 16:05:53 +1200

    Fixed fcreplay cloud functions

--- fcreplay/gcloud.py
+++ fcreplay/gcloud.py
@@ -3,8 +3,6 @@ import logging
 import os
 import requests
 
-import google.cloud.storage as storage
-
 with open("config.json", 'r') as json_data_file:
     config = json.load(json_data_file)
 
@@ -18,107 +16,6 @@ logging.basicConfig(
 
 REGION = config['gcloud_region']
 PROJECT_ID = config['gcloud_project']
-BUCKET_NAME = config['gcloud_bucket']
-
-# This requires: export GOOGLE_APPLICATION_CREDENTIALS="[PATH]"
-os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = config['gcloud_storage_creds_path']
-
-
-def upload_video(source_file_name, destination_file_name):
-    """Uploads a file to the bucket."""
-    storage_client = storage.Client()
-    bucket = storage_client.bucket(BUCKET_NAME)
-    blob = bucket.blob(destination_file_name)
-
-    blob.upload_from_filename(source_file_name)
-
-
-def download_video(source_file_name, destination_file_name):
-    """Downloads a blob from the bucket."""
-    storage_client = storage.Client()
-
-    bucket = storage_client.bucket(BUCKET_NAME)
-    blob = bucket.blob(source_file_name)
-    blob.download_to_filename(destination_file_name)
-
-
-def delete_video(file_name):
-    """Delete a blob from the bucket."""
-    storage_client = storage.Client()
-
-    bucket = storage_client.bucket(BUCKET_NAME)
-    blob = bucket.blob(file_name)
-    blob.delete()
-
-
-def launch_fcreplay():
-    logging.info("Starting launch_fcreplay")
-    RECEIVING_FUNCTION = 'launch_fcreplay'
-
-    function_url = f'https://{REGION}-{PROJECT_ID}.cloudfunctions.net/{RECEIVING_FUNCTION}'
-    metadata_server_url = \
-        'http://metadata/computeMetadata/v1/instance/service-accounts/default/identity?audience='
-    token_full_url = metadata_server_url + function_url
-    token_headers = {'Metadata-Flavor': 'Google'}
-
-    # Fetch the token
-    token_response = requests.get(token_full_url, headers=token_headers)
-    jwt = token_response.text
-
-    # Provide the token in the request to the receiving function
-    function_headers = {'Authorization': f'bearer {jwt}'}
-    function_response = requests.get(function_url, headers=function_headers)
-
-    logging.info(f"launch_fcreplay returned: {function_response.status_code}")
-    status = function_response.status_code
-    return status
-
-
-def launch_fcreplay_postprocessing():
-    logging.info("Starting launch_fcreplay_postprocessing")
-
-    RECEIVING_FUNCTION = 'launch_fcreplay_postprocessing'
-    function_url = f'https://{REGION}-{PROJECT_ID}.cloudfunctions.net/{RECEIVING_FUNCTION}'
-    metadata_server_url = \
-        'http://metadata/computeMetadata/v1/instance/service-accounts/default/identity?audience='
-    token_full_url = metadata_server_url + function_url
-    token_headers = {'Metadata-Flavor': 'Google'}
-
-    # Fetch the token
-    token_response = requests.get(token_full_url, headers=token_headers)
-    jwt = token_response.text
-
-    # Provide the token in the request to the receiving function
-    function_headers = {'Authorization': f'bearer {jwt}'}
-    function_response = requests.get(function_url, headers=function_headers)
-
-    logging.info(f"launch_fcreplay_postprocessing returned: {function_response.status_code}")
-    status = function_response.status_code
-    return(status)
-
-
-def destroy_fcreplay_postprocessing():
-    logging.info("Starting destroy_fcreplay_postprocessing")
-    RECEIVING_FUNCTION = 'destroy_fcreplay_postprocessing'
-
-    function_url = f'https://{REGION}-{PROJECT_ID}.cloudfunctions.net/{RECEIVING_FUNCTION}'
-    metadata_server_url = \
-        'http://metadata/computeMetadata/v1/instance/service-accounts/default/identity?audience='
-    token_full_url = metadata_server_url + function_url
-    token_headers = {'Metadata-Flavor': 'Google'}
-
-    # Fetch the token
-    token_response = requests.get(token_full_url, headers=token_headers)
-    jwt = token_response.text
-
-    # Provide the token in the request to the receiving function
-    function_headers = {'Authorization': f'bearer {jwt}'}
-    function_response = requests.get(function_url, headers=function_headers)
-
-    logging.info(f"destroy_fcreplay_postprocessing returned: {function_response.status_code}")
-    status = function_response.status_code
-    return(status)
-
 
 def destroy_fcreplay():
     logging.info("Starting destroy_fcreplay")
@@ -139,50 +36,5 @@ def destroy_fcreplay():
     function_response = requests.get(function_url, headers=function_headers)
 
     logging.info(f"destroy_fcreplay retruned: {function_response.status_code}")
-    status = function_response.status_code
-    return(status)
-
-def check_if_postprocessig_running():
-    logging.info("Starting check_if_postprocessing_running")
-    RECEIVING_FUNCTION = 'fcreplay_postprocessing_running'
-
-    function_url = f'https://{REGION}-{PROJECT_ID}.cloudfunctions.net/{RECEIVING_FUNCTION}'
-    metadata_server_url = \
-        'http://metadata/computeMetadata/v1/instance/service-accounts/default/identity?audience='
-    token_full_url = metadata_server_url + function_url
-    token_headers = {'Metadata-Flavor': 'Google'}
-
-    # Fetch the token
-    token_response = requests.get(token_full_url, headers=token_headers)
-    jwt = token_response.text
-
-    # Provide the token in the request to the receiving function
-    function_headers = {'Authorization': f'bearer {jwt}'}
-    function_response = requests.get(function_url, headers=function_headers)
-
-    logging.info(f"check_if_postprocessing_running retruned: {function_response.status_code}")
-    status = function_response.status_code
-    return(status)
-
-
-def check_if_running():
-    logging.info("Starting check_if_running")
-    RECEIVING_FUNCTION = 'fcreplay_running'
-
-    function_url = f'https://{REGION}-{PROJECT_ID}.cloudfunctions.net/{RECEIVING_FUNCTION}'
-    metadata_server_url = \
-        'http://metadata/computeMetadata/v1/instance/service-accounts/default/identity?audience='
-    token_full_url = metadata_server_url + function_url
-    token_headers = {'Metadata-Flavor': 'Google'}
-
-    # Fetch the token
-    token_response = requests.get(token_full_url, headers=token_headers)
-    jwt = token_response.text
-
-    # Provide the token in the request to the receiving function
-    function_headers = {'Authorization': f'bearer {jwt}'}
-    function_response = requests.get(function_url, headers=function_headers)
-
-    logging.info(f"check_if_running retruned: {function_response.status_code}")
     status = function_response.status_code
     return(status)
