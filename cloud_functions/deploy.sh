283e6a5cf8f8bf2f59ad60d91e40765efab33ddc
Gino Lisignoli <glisignoli@gmail.com>
2020-08-12 18:54:00 +1200

    Update instructions, remove terraform, add more deployment scripts

--- cloud_functions/deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-functions="check_for_replay destroy_fcreplay" 
-for f in $functions; do 
-    gcloud functions deploy $f --timeout=120 --entry-point $f --runtime python37 --trigger-http --source=./ --service-account=fcrecorder@appspot.gserviceaccount.com
-    if [ $? -gt 0 ]; then
-        exit 1
-    fi
-done
-exit 0
