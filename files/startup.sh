b3cfc8c79dc881a141df167e65ccf098fb1bec0a
Gino Lisignoli <glisignoli@gmail.com>
2021-01-02 18:53:46 +1300

    Fixed broken startup

--- files/startup.sh
+++ files/startup.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
 cd /root
-#wineboot --init
+
+# Needed to initalise i3 sockets
 export I3SOCK=$(i3 --get-socketpath)
-fcreplay instance 
+
+# Needed to resolve a bug that is in pyperclib (used by cmd2)
+# See: https://github.com/asweigart/pyperclip/issues/141 
+export XDG_SESSION_TYPE=x11
+
+# This sleep is needed so that the fcreplay-tasker container can attach
+# the extra networks needed for fcreplay to work
+sleep 2
+
+fcreplay instance
+
+# And these sleeps? I *think* they are needed for the python-loki-logging
+# module, so that it reads the file before the container is closed too quickly
 sleep 1
 echo '' >> /root/fcreplay.log
 sleep 1
